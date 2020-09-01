17c38af48d43c48c788f36c531c97d533ff19287
adam1279 <adamdamgolan@gmail.com>
2020-09-01 08:32:17 +0200

    what I have

---
+++ sketch.js
@@ -0,0 +1,40 @@
+var canvasInfo = {
+    height: 500,
+    width: 800,
+    color: 0
+}
+var walls = {
+    color: 200,
+    width: 10
+}
+new Wall({
+    color: walls.color,
+    width: walls.width,
+    x: 0,
+    goal: {
+        height: 100,
+        distance: 100
+    }
+});
+new Wall({
+    color: walls.color,
+    width: walls.width,
+    x: canvasInfo.width - walls.width,
+    goal: {
+        height: 100,
+        distance: 100
+    }
+});
+new Ground({
+    color: [0,255,0],
+    height: 10,
+    y: canvasInfo.height
+});
+function setup() {
+    createCanvas(canvasInfo.width, canvasInfo.height);
+}
+function draw() {
+    background(0);
+    Wall.instances.forEach(inst => inst.display());
+    Ground.instances.forEach(inst => inst.display());
+}
