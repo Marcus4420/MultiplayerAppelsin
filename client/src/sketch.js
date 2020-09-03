29569f845f88cecc9fd1334118930916f97d0865
Patrick Pedersen <patricksoeholtpedersen@gmail.com>
2020-09-03 17:22:53 +0200

    Canvas changes merged with Multiplayer changes. Readme updated.

--- client/src/sketch.js
+++ client/src/sketch.js
@@ -1,3 +1,6 @@
+let score1 = 0;
+let score2 = 0;
+
 var canvasInfo = {
     height: 500,
     width: 800,
@@ -7,7 +10,15 @@ var walls = {
     color: 200,
     width: 10
 }
-const gravity = 0.5;
+const gravity = 0.1;
+
+Array.prototype.removeItem = function(item) {
+    let index = this.indexOf(item);
+    if (index > -1) {
+        this.splice(index, 1);
+    }
+}
+
 new Wall({
     color: walls.color,
     width: walls.width,
@@ -54,6 +65,41 @@ new Player({
     }
 });
 function setup() {
-    let myCanvas = createCanvas(canvasInfo.width, canvasInfo.height);
-    myCanvas.parent("containerCanvas");
+    // Multiplayer
+    /*let myCanvas = createCanvas(canvasInfo.width, canvasInfo.height);
+    myCanvas.parent("containerCanvas");*/
+
+    // Non-Multiplayer
+    createCanvas(canvasInfo.width, canvasInfo.height);
+}
+function draw() {
+    background(0);
+    displayInstances([Wall, Ball, Ground, Player]);
+
+    fill(255);
+    text("Spiller 1:", 20, 20);
+    text("Score: "+score1, 20, 40);
+
+    text("Spiller 2:", canvasInfo.width - 80, 20);
+    text("Score: "+score2, canvasInfo.width - 80, 40);
+}
+
+setInterval(function() {
+    new Ball({
+        x: random(canvasInfo.width / 3, (canvasInfo.width / 3) * 2),
+        y: -50
+    })
+}, 5000);
+
+function displayInstances(x) {
+    if (Array.isArray(x)) {
+        for (var clss of x) {
+            clss.instances.forEach(inst => inst.display());
+        }
+    } else {
+        x.instances.forEach(inst => inst.display());
+    }
+}
+function toRadians (angle) {
+    return angle * (Math.PI / 180);
 }
